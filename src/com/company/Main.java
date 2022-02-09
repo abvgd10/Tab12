bb5dfa66715d4a698e6f7a4c015eaf8fde08a042
= <=>
2022-02-09 13:52:32 +0300

    qwe

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Uchbur uchbur = new Uchbur();
+        uchbur.setA(5);
+        uchbur.setB(5);
+        uchbur.setC(5);
+
+        int e = uchbur.q();
+        System.out.println(e);
+    }
+}
